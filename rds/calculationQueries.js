921525ec283a23a69abc804ebdbc948fa93612a2
Zachary Hoggard <zhoggard9@gmail.com>
2016-03-26 12:59:00 -0400

    Connection.pool implemented

--- rds/calculationQueries.js
+++ rds/calculationQueries.js
@@ -3,6 +3,7 @@ var connection = require('./connection');
 var calc = {
 
 	ldcID : function (ldcName, rateType, callback){
+    connection.getConnection(function(err, connection) {
    connection.query('SELECT id FROM LDC WHERE companyName = "'+ldcName+'" AND rateType = "'+rateType+'"', function(err, rows, fields) {
         if(err) {
           callback(err);
@@ -12,11 +13,14 @@ var calc = {
 	        rows.forEach(function(item){
 	        	returnId = item.id;
 	        });
+                connection.release();
 	        callback(returnId);
     	}
       });
+    });
   },	
   regConsumption : function (ldc, timePeriod, HOEPid, callback){
+    connection.getConnection(function(err, connection) {
    	connection.query('SELECT rateAmount FROM Rates WHERE units = "kWh" AND LDCid = '+ldc+' AND endDate IS null OR units="kWh" AND LDCid =' + HOEPid + ' AND startDate <= "'+timePeriod+'" AND endDate >= "'+timePeriod+'"', function(err, rows, fields) {
         if(err) {
           callback(err);
@@ -26,11 +30,14 @@ var calc = {
         	rows.forEach(function(item){
         		rateAmountSum = rateAmountSum + item.rateAmount;
         	});
+                connection.release();
           callback(rateAmountSum);
         }
       });
+    });
   },
   ToUConsumption : function (ldc, timeFrame, isWeekend, callback){
+    connection.getConnection(function(err, connection) {
     connection.query('SELECT rateAmount FROM TimeOfUse WHERE LDCid = '+ldc+' AND startHour <= "'+timeFrame+'" AND endHour >= "'+timeFrame+'" AND isWeekend = '+isWeekend+' AND endDate IS null', function(err, rows, fields) {
         if(err) {
           callback(err);
@@ -40,12 +47,15 @@ var calc = {
         	rows.forEach(function(item){
         		rateAmountSum = rateAmountSum + item.rateAmount;
         	});
+                connection.release();
           callback(rateAmountSum);
         }
       });
+    });
   },
   regDemand : function (ldc, callback){
   	//need to retrieve all the kWh recent rates from the Rates Table
+    connection.getConnection(function(err, connection) {
    connection.query('SELECT rateAmount FROM Rates WHERE units = "kW" AND LDCid = '+ldc, function(err, rows, fields) {
         if(err) {
           callback(err);
@@ -55,12 +65,15 @@ var calc = {
         	rows.forEach(function(item){
         		rateAmountSum = rateAmountSum + item.rateAmount;
         	});
+                connection.release();
           callback(rateAmountSum);
         }
       });
+    });
   },
   fixedCosts : function (ldc, callback){
   	//need to retrieve all the kWh recent rates from the Rates Table
+    connection.getConnection(function(err, connection) {
    connection.query('SELECT rateAmount FROM Rates WHERE units = "bill" AND LDCid = '+ldc, function(err, rows, fields) {
         if(err) {
           callback(err);
@@ -70,9 +83,11 @@ var calc = {
         	rows.forEach(function(item){
         		rateAmountSum = rateAmountSum + item.rateAmount;
         	});
+                connection.release();
           callback(rateAmountSum);
         }
       });
+    });
   }
 };
 
--- rds/connection.js
+++ rds/connection.js
@@ -2,7 +2,18 @@ var mysql = require('mysql');
 
 // Create connection with compardb configuration.
 // TODO: Should change this to use connection pooling.
+ /*
 var connection = mysql.createConnection({
+  host     : 'rateengine.chjpa4e5niju.us-west-2.rds.amazonaws.com',
+  user     : 'capstone',
+  password : 'capstone',
+  database : 'rateengdb', 
+  ssl      : 'Amazon RDS'
+});
+*/
+
+var connection = mysql.createPool({
+  connectionLimit : 15,
   host     : 'rateengine.chjpa4e5niju.us-west-2.rds.amazonaws.com',
   user     : 'capstone',
   password : 'capstone',
